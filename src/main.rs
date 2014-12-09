663314a8e2be7144ff69ed95fbd6d7b14eb8fbe7
Konstantin Stepanov <me@kstep.me>
2014-12-09 20:27:45 +0300

    fixes and outputs control

--- src/main.rs
+++ src/main.rs
@@ -27,50 +27,52 @@ fn json_result(s: &mut Stream, r: String) -> IoResult<()> {
     s.write_str(r[]))
 }
 
-fn run_player(s: &mut Stream, qs: TreeMap<String, String>, mpc: &mut MpdConnection) -> IoResult<()> {
+fn run_player(s: &mut Stream, qs: Option<TreeMap<String, String>>, mpc: &mut MpdConnection) -> IoResult<()> {
     debug!("params: {}", qs);
 
     s.write_str("Status: 200 OK\r\n").and_then(|()|
     s.write_str("Content-Type: application/json; charset=utf-8\r\n")).and_then(|()|
     s.write_str("\r\n")).and_then(|()| {
-        if let Some(cmd) = qs.get(&"cmd".into_string()) {
-            match cmd[] {
-                "next" => mpc.next(),
-                "prev" => mpc.prev(),
-                "set" => {
-                    if let Some(repeat) = qs.get(&"repeat".into_string()).and_then(|v| from_str(v[])) {
-                        mpc.set_repeat(repeat);
-                    }
-                    if let Some(single) = qs.get(&"single".into_string()).and_then(|v| from_str(v[])) {
-                        mpc.set_single(single);
-                    }
-                    if let Some(random) = qs.get(&"random".into_string()).and_then(|v| from_str(v[])) {
-                        mpc.set_random(random);
-                    }
-                    if let Some(consume) = qs.get(&"consume".into_string()).and_then(|v| from_str(v[])) {
-                        mpc.set_consume(consume);
-                    }
-                    if let Some(volume) = qs.get(&"volume".into_string()).and_then(|v| from_str(v[])) {
-                        mpc.set_volume(volume);
-                    }
-                    if let Some(elapsed_time) = qs.get(&"elapsed_time".into_string()).and_then(|v| from_str(v[])).map(|v| Duration::milliseconds(v)) {
-                        mpc.current_song().and_then(|ref mut s| s.seek(mpc, elapsed_time));
-                    }
-                    if let Some(state) = qs.get(&"state".into_string()) {
-                        match state[] {
-                            "Play" => match qs.get(&"id".into_string()).and_then(|v| from_str(v[])) {
-                                Some(id) => mpc.play_id(id),
-                                None => mpc.play()
-                            },
-                            "Pause" => mpc.pause(true),
-                            "Stop" => mpc.stop(),
-                            _ => Ok(())
-                        };
-                    }
-                    Ok(())
-                },
-                _ => Ok(())
-            };
+        if let Some(ref qs) = qs {
+            if let Some(cmd) = qs.get(&"cmd".into_string()) {
+                match cmd[] {
+                    "next" => mpc.next(),
+                    "prev" => mpc.prev(),
+                    "set" => {
+                        if let Some(repeat) = qs.get(&"repeat".into_string()).and_then(|v| from_str(v[])) {
+                            mpc.set_repeat(repeat);
+                        }
+                        if let Some(single) = qs.get(&"single".into_string()).and_then(|v| from_str(v[])) {
+                            mpc.set_single(single);
+                        }
+                        if let Some(random) = qs.get(&"random".into_string()).and_then(|v| from_str(v[])) {
+                            mpc.set_random(random);
+                        }
+                        if let Some(consume) = qs.get(&"consume".into_string()).and_then(|v| from_str(v[])) {
+                            mpc.set_consume(consume);
+                        }
+                        if let Some(volume) = qs.get(&"volume".into_string()).and_then(|v| from_str(v[])) {
+                            mpc.set_volume(volume);
+                        }
+                        if let Some(elapsed_time) = qs.get(&"elapsed_time".into_string()).and_then(|v| from_str(v[])).map(|v| Duration::milliseconds(v)) {
+                            mpc.current_song().and_then(|ref mut s| s.seek(mpc, elapsed_time));
+                        }
+                        if let Some(state) = qs.get(&"state".into_string()) {
+                            match state[] {
+                                "Play" => match qs.get(&"id".into_string()).and_then(|v| from_str(v[])) {
+                                    Some(id) => mpc.play_id(id),
+                                    None => mpc.play()
+                                },
+                                "Pause" => mpc.pause(true),
+                                "Stop" => mpc.stop(),
+                                _ => Ok(())
+                            };
+                        }
+                        Ok(())
+                    },
+                    _ => Ok(())
+                };
+            }
         }
         Ok(())
     }).and_then(|()|
@@ -97,6 +99,18 @@ fn run_queue(s: &mut Stream, method: String, qs: TreeMap<String, String>, mpc: &
     })
 }
 
+fn run_outputs(s: &mut Stream, qs: Option<TreeMap<String, String>>, mpc: &mut MpdConnection) -> IoResult<()> {
+    let outputs = mpc.outputs();
+    if let Some(ref qs) = qs {
+        if let Some(enabled) = qs.get(&"enabled".into_string()).and_then(|v| from_str(v[])) {
+            if let Some(id) = qs.get(&"id".into_string()).and_then(|v| from_str(v[])) {
+                outputs.set(id, enabled);
+            }
+        }
+    }
+    json_result(s, json::encode(&outputs))
+}
+
 fn run(s: &mut Stream, env: &SCGIEnv) -> IoResult<()> {
 
     let mut mpc = MpdConnection::new(None, 6600).unwrap().unwrap();
@@ -110,9 +124,9 @@ fn run(s: &mut Stream, env: &SCGIEnv) -> IoResult<()> {
                 Some(b"plugins") => match parts.next() {
                     Some(b"mpd") => match parts.next() {
                         Some(b"queue.json") => run_queue(s, env.get("REQUEST_METHOD").unwrap(), env.query().unwrap(), &mut mpc),
-                        Some(b"player.json") => run_player(s, env.query().unwrap(), &mut mpc),
+                        Some(b"player.json") => run_player(s, env.query(), &mut mpc),
                         Some(b"current-song.json") => json_result(s, json::encode(&mpc.current_song())),
-                        Some(b"outputs.json") => json_result(s, json::encode(&mpc.outputs())),
+                        Some(b"outputs.json") => run_outputs(s, env.query(), &mut mpc),
                         Some(b"playlists.json") => json_result(s, json::encode(&mpc.playlists())),
                         _ => not_found(s)
                     },
